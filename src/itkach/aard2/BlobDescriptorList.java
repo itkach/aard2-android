0cec2d44be891af0fe6299208970b11158ff74ff
Igor Tkach <itkach@aarddict.org>
2014-12-13 17:51:27 -0500

    Use QUATERNARY comparator instead of IDENTICAL for bookmark/history title sorting

--- src/itkach/aard2/BlobDescriptorList.java
+++ src/itkach/aard2/BlobDescriptorList.java
@@ -62,7 +62,7 @@ final class BlobDescriptorList extends AbstractList<BlobDescriptor> {
         this.filteredList = new ArrayList<BlobDescriptor>();
         this.dataSetObservable = new DataSetObservable();
         this.filter = "";
-        keyComparator = Slob.COMPARATORS.get(Slob.Strength.IDENTICAL);
+        keyComparator = Slob.COMPARATORS.get(Slob.Strength.QUATERNARY);
 
         nameComparatorAsc = new Comparator<BlobDescriptor>() {
             @Override
