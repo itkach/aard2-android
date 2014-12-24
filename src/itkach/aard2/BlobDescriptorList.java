b5cb8fc39671c395be086bf6255fc3fd458af4d7
Igor Tkach <itkach@aarddict.org>
2014-12-23 22:38:12 -0500

    Don't crash if sorting bookmarks or history by title fails

--- src/itkach/aard2/BlobDescriptorList.java
+++ src/itkach/aard2/BlobDescriptorList.java
@@ -141,7 +141,37 @@ final class BlobDescriptorList extends AbstractList<BlobDescriptor> {
     }
 
     private void sortOrderChanged() {
-        Collections.sort(this.filteredList, comparator);
+        try {
+            Collections.sort(this.filteredList, comparator);
+        }
+        catch(Exception e) {
+            //From http://www.oracle.com/technetwork/java/javase/compatibility-417013.html#source
+            /*
+            Synopsis: Updated sort behavior for Arrays and Collections may throw an IllegalArgumentException
+            Description: The sorting algorithm used by java.util.Arrays.sort and (indirectly) by
+                         java.util.Collections.sort has been replaced. The new sort implementation may
+                         throw an IllegalArgumentException if it detects a Comparable that violates
+                         the Comparable contract. The previous implementation silently ignored such a situation.
+                         If the previous behavior is desired, you can use the new system property,
+                         java.util.Arrays.useLegacyMergeSort, to restore previous mergesort behavior.
+            Nature of Incompatibility: behavioral
+            RFE: 6804124
+             */
+            //Name comparators use ICU collation key comparison. Given Unicode collation complexity
+            //it's hard to be sure that collation key comparisons won't trigger an exception. It certainly
+            //does at least for some keys in ICU 53.1.
+            //Incorrect or no sorting seems preferable than a crashing app.
+            //TODO perhaps java.util.Collections.sort shouldn't be used at all
+
+            Log.w(TAG, "Error while sorting descriptors:", e);
+            StringBuffer sb = new StringBuffer("Descriptors keys that caused sort failure:");
+            for (BlobDescriptor bd : this.filteredList) {
+                sb.append("\n");
+                sb.append(bd.key);
+            }
+            Log.w(TAG, sb.toString());
+        }
+
         this.dataSetObservable.notifyChanged();
     }
 
