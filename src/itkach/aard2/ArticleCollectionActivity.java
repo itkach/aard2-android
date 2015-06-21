cee15989f275a1f81fdfe69ccb2792a75a33cdc4
Igor Tkach <itkach@aarddict.org>
2015-06-21 10:47:36 -0400

    Remove obsolete header comment

--- src/itkach/aard2/ArticleCollectionActivity.java
+++ src/itkach/aard2/ArticleCollectionActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package itkach.aard2;
 
 import android.app.ActionBar;
@@ -25,7 +9,6 @@ import android.content.SharedPreferences;
 import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -45,9 +28,7 @@ import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 import itkach.slob.Slob;
 import itkach.slob.Slob.Blob;
